f4a2b788c8b4f127ef052ee11f5535854011d2a7
Mert AHMETOĞLU <64382208+asikLEMMY@users.noreply.github.com>
2021-06-17 00:18:37 +0300

    mainly changes at starting of code block

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function discord_message_send($webhook_url, $data) {
+    if (!array_key_exists('avatar_url', $data))
+        $data['avatar_url'] = 'nothing.test';
 
+
+    if (!array_key_exists('username', $data))
+        $data['username'] = "nothing.test";
+
+    $session_curl = curl_init('https://discord.com/api/webhooks/', $webhook_url);
+
+    return $session_curl;
+
+}
+
+
+// just new session, I will at it tho new file soon. 
