273cd82d6c98fd4711f1fa2bf544323de1f726ac
Mert AHMETOĞLU <64382208+asikLEMMY@users.noreply.github.com>
2021-06-23 23:41:48 +0300

    controling reactions at messages, and deleting.
    simply ts code, of deleting and adding reactions at messages.

---
+++ reaction.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+const user = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
+
+user.on('messageReactionAdd', async (reaction,user) => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+
+        } catch(error) {
+            console.log(`Birşeyler yanlış gitti. Hata:`, error);
+            return;
+        }
+
+    }
+
+    console.log(`${reaction.message.author}'n mesajı "${reaction.message.content}" reaksiyon kazandı!`);
+    
+    // end
+    
+});
+
+// adding some admin commands.
+
+user.on('message', message => {
+    if (user.hasPermission('MANAGE_MESSAGES')) {
+    if (message.content == '!reaksiyonsil') {
+        message.reactions.removeAll();
+        message.reply('Mesajlardan reaksiyonları başarıyla sildin.');
+    } else {
+        message.reply('Yeteri yetkiniz yok.');
+    }
+  }
+});
+
+
+
+
