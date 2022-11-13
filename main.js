0bf9bdd25b21cc174aa0110403cd7007eb4bb928
dipdipper <51788461+dipdipper@users.noreply.github.com>
2022-11-13 16:19:55 +0300

    Update main.js
    Now you can run bot in newer versions.

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const Discord = require('discord.js');
+const { Client, GatewayIntentBits, PermissionsBitField, Permissions } = require('discord.js');
+
 const { prefix, token } = require('./config.json');
-const ytdl = require('ytdl-core');
 
-const client = new Discord.Client();
-
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 const activities = [
   "Left This One Empty",
